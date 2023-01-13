e4d844d9bba76ffda361084d65ac074c5a7de2b0
jenersigua31 <79560699+jenersigua31@users.noreply.github.com>
2023-01-13 17:03:43 +0800

    Update useBEM.ts

--- src/hooks/useBEM.ts
+++ src/hooks/useBEM.ts
@@ -1,40 +1,42 @@
-type bemType = [
-    B: (modifierName?: string | string[]) => string,
-    E: (elementName: string, modifiers?: string | string[]) => string,
-]
-
-const useBEM  = (blockName: string): bemType =>  {
-
-        const E = (elementName: string, modifiers?: string | string[]): string => {
-
-            let modifier = '';
-            if (modifiers) {
-                modifier = `${blockName}__${elementName}--${modifiers}`;
-                if(Array.isArray(modifiers)){
-                    modifier = modifiers.map( m => m ? `${blockName}__${elementName}--${m}` : '').join(' ');
-                }
-            }
-
-            return `${blockName}__${elementName} ${modifier}`.trim();
-        }
-
-        const B = (modifiers?: string | string[]): string => {         
-            
-            let modifier = '';
-            if (modifiers) {
-                modifier = `${blockName}--${modifiers}`;
-                if (Array.isArray(modifiers)) {
-                    modifier = modifiers.map( m => m ? `${blockName}--${m}` : '').join(' ');
-                }
-            }
-            
-            return `${blockName} ${modifier}`.trim();
-        }
-
-        return [
-            B,
-            E
-        ]
+const modifierGenerator = (cls: string, modifier: string | string[]) => {
+    const mods = Array.isArray(modifier) ? modifier : [modifier];
+    return mods.map( m => `${cls}--${m}`).join(' ');
 }
 
-export default useBEM;
+interface otherClass {
+    modifier?: string | string[];
+    def?: string
+}
+
+const classGenerator = (cls: string, others?: otherClass) => {
+    const defaultClass = (others?.def || '');
+    const mods = (others?.modifier && modifierGenerator(cls, others?.modifier)) || '';
+    return `${defaultClass} ${cls} ${mods}`.trim();
+}
+
+const useBEM = (block: string, className?: string) => {
+
+    const B = (modifier?: string | string[]) => {
+        return classGenerator(block, {
+            def: (className || ''),
+            modifier
+        });
+    }
+
+    const E = (element: string, modifier?: string | string[], def?: string) => {
+        const BE = `${block.split(' ')[0]}__${element}`;
+        return classGenerator(BE, {
+            def: (def || ''),
+            modifier
+        });
+    }
+
+    return {
+        B,
+        E
+    }
+}
+
+export {
+    useBEM
+};
